7c1f8d8c6e87bcd6fbbab8708be3da12dc9f51f2
nakanaka3 <160567129+nakanaka3@users.noreply.github.com>
2024-05-26 13:35:36 +0900

    update

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,27 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss
+{
+    int mp = 53;// 魔法力
+
+    // 魔法攻撃の関数
+    public void Magic()
+    {
+        if (mp >= 5)
+        {
+            mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+        }
+        else
+        {
+            Debug.Log("MPが足りないため、魔法が使えない。");
+        }
+    }
+}
+
 public class Test : MonoBehaviour
 {
 
     // Start is called before the first frame update
     void Start()
     {
-        // 配列を初期化する
-        int[] points = { 10, 20, 30, 40, 50 };
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss x = new Boss();
 
-        // 配列の要素数のぶんだけ1ずつ足す処理を繰り返す
-        for (int i = 0; i < points.Length; i++)
-        {
-            // 配列の要素を表示する
-            Debug.Log(points[i]);
-        }
-        // 配列の要素数のぶんだけ１ずつ減らす処理を繰り返す
-        for (int i = 4; i >= 0; i--)
-        {
-            // 配列の要素を表示する
-            Debug.Log(points[i]);
-        }
+        // 魔法攻撃用の関数を呼び出す
+        x.Magic();
+        x.Magic();
+        x.Magic();
+        x.Magic();
+        x.Magic();
+        x.Magic();
+        x.Magic();
+        x.Magic();
+        x.Magic();
+        x.Magic();
+        x.Magic();
     }
 
     // Update is called once per frame
